eefb96ec6bee88a727a6c29494527f44ad4c31f5
SaaSha <saasha.pina@gmail.com>
2018-06-27 19:15:40 -0400

    added category

--- notes/models.py
+++ notes/models.py
@@ -8,5 +8,6 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=20)
     #TODO: Tagging or categories
 
